aa514eab609bc32d4efd22f4fa608f84320d1828
alexvujisic <alexandar.vujisic@stud.fh-campuswien.ac.at>
2024-11-15 15:33:29 +0100

    refactor and new wall texture

--- main.js
+++ main.js
@@ -11,32 +11,31 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const lights = new Lights()
-scene.add(lights.ambientLight)
-scene.add(lights.hemisphereLight)
-scene.add(lights.directionalLight)
-scene.add(lights.pointLight)
+lightSetup();
 
 const world = new World(camera, scene)
 
 function animate(){
-    world.animate()
     renderer.render(scene, camera)
 }
 renderer.setAnimationLoop(animate)
 
-// Kamera Positionieren
 camera.position.z = 0;
 
 // Boden und Wände erstellen
-const wallMaterial = new THREE.MeshPhongMaterial({ color: 0xac7339, side: THREE.DoubleSide });
-
 const textureLoader = new THREE.TextureLoader();
 
 const ceilTexture = textureLoader.load('photos/il_fullxfull.4227498063_dlzn.jpg.webp');  //deckenbild
 
 const floorTexture = textureLoader.load('photos/undef_src_sa_picid_794028_x_1000_type_whitesh_quality_100_image.jpg'); //marmorboden
 
+const wallTexture = textureLoader.load('photos/Schwarzer Marmor Quelle jpkirakun.jpg');
+
+const wallMaterial = new THREE.MeshPhongMaterial({
+    map: wallTexture,
+    //color: 0xac7339, 
+    side: THREE.DoubleSide
+});
 // Boden
 const floorGeometry = new THREE.PlaneGeometry( 50, 50 );  
 
@@ -64,81 +63,14 @@ const ceilMaterial = new THREE.MeshPhongMaterial({
 });
 const ceil = new THREE.Mesh( ceilGeometry, ceilMaterial );
 
-
 ceil.rotation.x = Math.PI / 2;
 ceil.position.y = 3
 
 ceil.receiveShadow = true;
 scene.add( ceil );
 
-
-// Wände
-const wallGeometry = new THREE.PlaneGeometry(15, 6);
-
-// Linke Wand
-const leftWall = new THREE.Mesh(wallGeometry, wallMaterial);
-leftWall.position.x = -7.5;
-leftWall.rotation.y = Math.PI / 2;
-scene.add(leftWall);
-
-// Rechte Wand
-const rightWall = new THREE.Mesh(wallGeometry, wallMaterial);
-rightWall.position.x = 7.5;
-rightWall.rotation.y = -Math.PI / 2;
-scene.add(rightWall);
-
-// Hintere Wand
-const backWall = new THREE.Mesh(wallGeometry, wallMaterial);
-backWall.position.z = -7.5;
-scene.add(backWall);
-
-//Vordere Wand
-const frontWall = new THREE.Mesh(wallGeometry, wallMaterial);
-frontWall.position.z = 7.5;
-scene.add(frontWall);
-
-
-
-
-
-
-// Bild an der vordere Wand
-textureLoader.load('photos/monalisa.jpg', (texture) => {
-    const pictureMaterial = new THREE.MeshBasicMaterial({ map: texture });
-    const picture = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), pictureMaterial);
-    picture.position.set(0, 0.5, -7.4);
-    scene.add(picture);
-});
-
-// Bild an der rechten Wand
-textureLoader.load('photos/edvard-munch-schrei-beruehmtes-gemaelde.jpg', (texture) => {
-    const pictureMaterial = new THREE.MeshBasicMaterial({ map: texture });
-    const picture = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), pictureMaterial);
-    picture.position.set(7.4, 0.5, 0);
-    picture.rotation.y = -Math.PI / 2;
-    
-    scene.add(picture);
-});
-
-// Bild an der linke Wand
-textureLoader.load('photos/csm_leonardo-da-vinci-vitruvianischer-mensch_3befc41428.jpg.pagespeed.ce.FgwBSJ3N-D.jpg', (texture) => {
-    const pictureMaterial = new THREE.MeshBasicMaterial({ map: texture });
-    const picture = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), pictureMaterial);
-    picture.position.set(-7.4, 0.5, 0);
-    picture.rotation.y = Math.PI / 2;
-    scene.add(picture);
-    
-});
-
-//hintere wand
-textureLoader.load('photos/van-Gogh-Sternennacht.jpg', (texture) => {
-    const pictureMaterial = new THREE.MeshBasicMaterial({ map: texture });
-    const picture = new THREE.Mesh(new THREE.PlaneGeometry(3, 3), pictureMaterial);
-    picture.position.set(0, 0.5, 7.4);
-    picture.rotation.y = 3.14
-    scene.add(picture);
-});
-
+wallSetup();
+pictureSetup();
 
 // Steuerung der Kamera aufrufen
 setupControls(camera, renderer, scene);
@@ -149,3 +81,81 @@ window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 });
+
+function ceilSetup(){
+    
+}
+
+function wallSetup(){
+    // Wände
+    const wallGeometry = new THREE.PlaneGeometry(15, 6);
+
+    // Linke Wand
+    const leftWall = new THREE.Mesh(wallGeometry, wallMaterial);
+    leftWall.position.x = -7.5;
+    leftWall.rotation.y = Math.PI / 2;
+    scene.add(leftWall);
+
+    // Rechte Wand
+    const rightWall = new THREE.Mesh(wallGeometry, wallMaterial);
+    rightWall.position.x = 7.5;
+    rightWall.rotation.y = -Math.PI / 2;
+    scene.add(rightWall);
+
+    // Hintere Wand
+    const backWall = new THREE.Mesh(wallGeometry, wallMaterial);
+    backWall.position.z = -7.5;
+    scene.add(backWall);
+
+    //Vordere Wand
+    const frontWall = new THREE.Mesh(wallGeometry, wallMaterial);
+    frontWall.position.z = 7.5;
+    scene.add(frontWall);
+}
+
+function pictureSetup(){
+    // Bild an der vordere Wand
+    textureLoader.load('photos/monalisa.jpg', (texture) => {
+        const pictureMaterial = new THREE.MeshBasicMaterial({ map: texture });
+        const picture = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), pictureMaterial);
+        picture.position.set(0, 0.5, -7.4);
+        scene.add(picture);
+    });
+
+    // Bild an der rechten Wand
+    textureLoader.load('photos/edvard-munch-schrei-beruehmtes-gemaelde.jpg', (texture) => {
+        const pictureMaterial = new THREE.MeshBasicMaterial({ map: texture });
+        const picture = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), pictureMaterial);
+        picture.position.set(7.4, 0.5, 0);
+        picture.rotation.y = -Math.PI / 2;
+        
+        scene.add(picture);
+    });
+
+    // Bild an der linke Wand
+    textureLoader.load('photos/csm_leonardo-da-vinci-vitruvianischer-mensch_3befc41428.jpg.pagespeed.ce.FgwBSJ3N-D.jpg', (texture) => {
+        const pictureMaterial = new THREE.MeshBasicMaterial({ map: texture });
+        const picture = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), pictureMaterial);
+        picture.position.set(-7.4, 0.5, 0);
+        picture.rotation.y = Math.PI / 2;
+        scene.add(picture);
+        
+    });
+
+    //hintere wand
+    textureLoader.load('photos/van-Gogh-Sternennacht.jpg', (texture) => {
+        const pictureMaterial = new THREE.MeshBasicMaterial({ map: texture });
+        const picture = new THREE.Mesh(new THREE.PlaneGeometry(3, 3), pictureMaterial);
+        picture.position.set(0, 0.5, 7.4);
+        picture.rotation.y = 3.14
+        scene.add(picture);
+    });
+}
+
+function lightSetup(){
+    const lights = new Lights()
+    scene.add(lights.ambientLight)
+    scene.add(lights.hemisphereLight)
+    scene.add(lights.directionalLight)
+    scene.add(lights.pointLight)
+}
--- src/lights.js
+++ src/lights.js
@@ -3,26 +3,18 @@ import * as THREE from 'three'
 class Lights {
 
     constructor(){
-
-
-
         this.ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-
 
         this.hemisphereLight = new THREE.HemisphereLight(0xaaaaff, 0x555555, 0.6);
         this.hemisphereLight.position.set(0, 10, 0); // Über der Szene positioniert
 
-
         this.directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
-
         this.directionalLight.position.set(5, 10, 5); 
         this.directionalLight.castShadow = true; 
         this.directionalLight.shadow.mapSize.width = 1024; 
         this.directionalLight.shadow.mapSize.height = 1024;
 
-
         this.pointLight = new THREE.PointLight(0xffddaa, 1, 50);
-
         this.pointLight.position.set(0, 5, 0); 
     }
 }
